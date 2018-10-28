394f2f96643dde7c9ed84abdb6a7062390f3d600
pereyrac <christian.pereyra.ext@boehringer-ingelheim.com>
2018-10-27 22:05:05 -0300

    Initial commit

---
+++ src/main/java/com/user/api_challenge/ApiChallengeApplication.java
@@ -0,0 +1,12 @@
+package com.user.api_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiChallengeApplication.class, args);
+	}
+}
